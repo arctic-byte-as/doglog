12324c53a9e914e8f343cc6dd0fcad10b21e5db1
“Ian <idr.robertson@gmail.com>
2026-05-24 11:34:57 +0200

    update to edit

---
+++ prisma/migrations/20260524092829_add_consultation_fields/migration.sql
@@ -0,0 +1,11 @@
+-- AlterTable
+ALTER TABLE "Consultation" ADD COLUMN "activation" TEXT;
+ALTER TABLE "Consultation" ADD COLUMN "dogBreed" TEXT;
+ALTER TABLE "Consultation" ADD COLUMN "generalDescription" TEXT;
+ALTER TABLE "Consultation" ADD COLUMN "health" TEXT;
+ALTER TABLE "Consultation" ADD COLUMN "hormoneAnalysis" TEXT;
+ALTER TABLE "Consultation" ADD COLUMN "learningHistory" TEXT;
+ALTER TABLE "Consultation" ADD COLUMN "nutrition" TEXT;
+ALTER TABLE "Consultation" ADD COLUMN "prescribedPlan" TEXT;
+ALTER TABLE "Consultation" ADD COLUMN "situation" TEXT;
+ALTER TABLE "Consultation" ADD COLUMN "stimulusAnalysis" TEXT;
